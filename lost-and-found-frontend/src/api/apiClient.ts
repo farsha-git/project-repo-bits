fcfe062c5c83331e1c820d53d0d85aa8bbe04d32
farsha-git <farshaassaraf@gmail.com>
2025-12-11 06:31:21 -0500

    Update apiClient.ts

--- lost-and-found-frontend/src/api/apiClient.ts
+++ lost-and-found-frontend/src/api/apiClient.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_API_BASE || "http://65.1.134.8:4000/api",
   headers: { "Content-Type": "application/json" },
   withCredentials: false,
 });
